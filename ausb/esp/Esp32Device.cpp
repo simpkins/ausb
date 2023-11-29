0d0fc7ed6ab0336571d74827277c35e534dc2dac
Adam Simpkins <adam@adamsimpkins.net>
2023-11-29 15:14:02 -0800

    Initial commit

---
+++ ausb/esp/Esp32Device.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2023, Adam Simpkins
+#include "ausb/esp/Esp32Device.h"
+
+namespace ausb {
+
+} // namespace ausb
---
+++ ausb/esp/Esp32Device.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2023, Adam Simpkins
+#pragma once
+
+namespace ausb {
+
+class Esp32Device {
+public:
+private:
+  // Forbidden copy constructor and assignment operator
+  Esp32Device(Esp32Device const &) = delete;
+  Esp32Device &operator=(Esp32Device const &) = delete;
+};
+
+} // namespace ausb
